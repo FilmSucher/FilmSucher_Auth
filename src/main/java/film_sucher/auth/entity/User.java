cd1e1002c7aeedf8920b35a6ccafbc0dec800b72
VaStan96 <vastan96@gmail.com>
2025-08-12 12:13:51 +0200

    fix DBConnections problem 6

--- src/main/java/film_sucher/auth/entity/User.java
+++ src/main/java/film_sucher/auth/entity/User.java
@@ -7,15 +7,17 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="Users")
+@Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq_gen")
+    @SequenceGenerator(name = "users_seq_gen", sequenceName = "users_seq", allocationSize = 1)
     private Long id;
 
     private String username;
